d8e93a6c088fba7e7a93965347fbdc2f467003d4
RabidChinchilla <bengearing5@gmail.com>
2018-10-20 17:41:26 +0100

    made 1st testing range
    created a scene, populated it with targets, added a floor panel to change scene but need to work on the script

---
+++ Weapons testing/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour {
+
+    private int nextIndex;
+
+    void start()
+    {
+        //nextIndex = SceneManager.GetActiveScene;
+    }
+
+	void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
