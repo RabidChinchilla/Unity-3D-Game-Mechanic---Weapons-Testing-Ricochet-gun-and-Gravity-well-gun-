c66ef10c9967fbc7c1721853a6a63db0a82364ac
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2018-11-02 16:14:45 +0000

    added some coments to the gravit well code

--- Weapons testing/Assets/Scripts/GravityWell.cs
+++ Weapons testing/Assets/Scripts/GravityWell.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GravityWell : MonoBehaviour {
 
+    //use a negative force to create an implosion
     public float force = -100.0f;
     public float radius = 5.0f;
 
@@ -15,12 +16,14 @@ public class GravityWell : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        //call in update to simulate gravity rather than just a once off implosion
         Vector3 explosionPosition = transform.position;
         Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
         foreach (Collider hit in colliders)
         {
             Rigidbody m_rigidBody = hit.GetComponent<Rigidbody>();
-
+            
+            //if the object in the radius has a rigid body apply the imposion for and pull it towards the gravity well
             if (m_rigidBody != null)
             {
                 m_rigidBody.AddExplosionForce(force, explosionPosition, radius);
