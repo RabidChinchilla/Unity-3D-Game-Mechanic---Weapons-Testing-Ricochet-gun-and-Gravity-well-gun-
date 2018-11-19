c0ea5bfac360e23c70c3b1048f68e3222419c3e6
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2018-11-19 13:35:29 +0000

    further gameplay development
    added an animation for recoil but still need to sort out the script so it plays on click, also changed some of the settings of the platforms so they would send up where they need to be, also added the cross hair to the ui

---
+++ Weapons testing/Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PlatformPoint")
+        {
+            rb.isKinematic = true;
+        }
+        
+    }
+}
