e56de7e332c280bddab5ad63aa7302c68c57ad88
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2018-11-20 11:07:24 +0000

    Demo session
    got some feeback and made some quick changes to the cameras

---
+++ Weapons testing/Assets/Scripts/ExtraCams.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraCams : MonoBehaviour {
+
+    public Camera cam1;
+    public Camera cam2;
+	
+	// Update is called once per frame
+	void Update () {
+
+        cam1.rect = new Rect(0.8f, 0, 0.2f, 0.2f);
+        cam2.rect = new Rect(0.8f, 0.8f, 0.2f, 0.2f);
+	}
+}
