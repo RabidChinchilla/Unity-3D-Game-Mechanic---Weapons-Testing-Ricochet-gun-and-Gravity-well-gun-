589ef335d834f043694995b4248c0e4cd0e5abe5
RabidChinchilla <bengearing5@gmail.com>
2018-12-30 21:12:31 +0000

    bug fixes and making level more interesting
    made the first level more interesting by making some panels move using animation, also changed the target explosion so it uses a particle effect rather than creating objects and using explosion force

--- Weapons testing/Assets/Scripts/ExplosionPoint.cs
+++ Weapons testing/Assets/Scripts/ExplosionPoint.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class ExplosionPoint : MonoBehaviour {
 
-    public float force = -50.0f;
-    public float radius = 10.0f;
+    //public float force = -50.0f;
+    //public float radius = 10.0f;
 
-    public GameObject fragment;
+    //public GameObject fragment;
 
     // Use this for initialization
     void Start () {
 
-        for (int i = 0; i < 10; i++)
-        {
-            Instantiate(fragment, transform.position, transform.rotation);
-        }
+        //for (int i = 0; i < 10; i++)
+        //{
+            //Instantiate(fragment, transform.position, transform.rotation);
+        //}
 
-        Vector3 explosionPosition = transform.position;
-        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
-        foreach (Collider hit in colliders)
-        {
-            Rigidbody m_rigidBody = hit.GetComponent<Rigidbody>();
+        //Vector3 explosionPosition = transform.position;
+        //Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+        //foreach (Collider hit in colliders)
+        //{
+            //Rigidbody m_rigidBody = hit.GetComponent<Rigidbody>();
 
-            if (m_rigidBody != null)
-            {
-                m_rigidBody.AddExplosionForce(force, explosionPosition, radius);
-            }
-        }
+            //if (m_rigidBody != null)
+            //{
+                //m_rigidBody.AddExplosionForce(force, explosionPosition, radius);
+            //}
+        //}
 
         Object.Destroy(gameObject, 3.0f);
     }
