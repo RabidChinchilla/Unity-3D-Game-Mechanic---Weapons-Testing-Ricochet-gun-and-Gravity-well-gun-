0587054da99cb27e207bf7e95ec6dfd2d30577f7
RabidChinchilla <bengearing5@gmail.com>
2019-01-02 18:48:20 +0000

    added more colliders
    after the usability test i realised players abuse the fact they can pull the box towards them so i added colliders that stop that

---
+++ Weapons testing/Assets/Scripts/resetbox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetbox : MonoBehaviour
+{
+
+    public GameObject box;
+    public Vector3 originalPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        originalPosition = box.transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Box")
+        {
+            box.transform.position = originalPosition;
+        }
+    }
+}
