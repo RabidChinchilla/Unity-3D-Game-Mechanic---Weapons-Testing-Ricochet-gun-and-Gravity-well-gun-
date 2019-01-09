51065aa9828e494a2f0e1bfe7e50d6d699ee01c4
RabidChinchilla <bengearing5@gmail.com>
2019-01-09 22:50:20 +0000

    work on usability feedback
    after taking ideas from the usability test i have changed the lighting, extended the platforming part, and fleshed out the main menu

--- Weapons testing/Assets/Scripts/ChangeLevel.cs
+++ Weapons testing/Assets/Scripts/ChangeLevel.cs
@@ -7,18 +7,21 @@ public class ChangeLevel : MonoBehaviour {
 
     public int nextIndex;
     public GameObject box;
+    public GameObject Player;
+    public Vector3 PlayerPosition;
     public Vector3 originalPosition;
 
     void Start()
     {
         originalPosition = box.transform.position;
+        PlayerPosition = Player.transform.position;
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
         {
-            SceneManager.LoadScene(nextIndex);
+            other.transform.position = PlayerPosition;
         }
         else if (other.tag == "Box")
         {
