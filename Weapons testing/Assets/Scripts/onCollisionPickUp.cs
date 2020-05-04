806f457d940c594419ae8e21baf7c1c565e6e712
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2020-05-04 23:54:13 +0100

    weapon swapping and pick ups

---
+++ Weapons testing/Assets/Scripts/onCollisionPickUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onCollisionPickUp : MonoBehaviour {
+
+	public GameObject item;
+	private GameObject player;
+	private swapWeapons weaponSwap;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		weaponSwap = player.GetComponent<swapWeapons>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		//Debug.Log("Collision detected");
+		item.SetActive(true);
+
+		if (this.gameObject.tag == "gun1")
+		{
+			weaponSwap.gun1Obtained = true;
+			Debug.Log("Weapon 1 obtained");
+		}
+
+		if (this.gameObject.tag == "gun2")
+		{
+			weaponSwap.gun2Obtained = true;
+			Debug.Log("Weapon 2 obtained");
+		}
+
+
+		Destroy(this.gameObject);
+	}
+}
---
+++ Weapons testing/Assets/Scripts/swapWeapons.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swapWeapons : MonoBehaviour {
+
+	public GameObject gun1;
+	public GameObject gun2;
+
+	public bool gun1Obtained;
+	public bool gun2Obtained;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			print("1 key was pressed");
+			if (gun1Obtained == true)
+			{
+				gun2.SetActive(false);
+				gun1.SetActive(true);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			if (gun2Obtained == true)
+			{
+				print("1 key was pressed");
+				gun1.SetActive(false);
+				gun2.SetActive(true);
+			}
+		}
+	}
+}
